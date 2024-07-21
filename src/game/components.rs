98db7e510b6c838556f54604d1b45b63829954f8
ickshonpe <david.curthoys@googlemail.com>
2024-07-21 21:31:20 +0100

    Added types representing terrain and unit special abilities

--- src/game/components.rs
+++ src/game/components.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy::math::UVec2;
+use bevy::utils::HashSet;
 
 
 /// amount of damage a unit can take before dying
@@ -27,12 +28,45 @@ pub struct Position(pub UVec2);
 
 /// Has unit moved or performed an action yet
 /// Needs to be reset to default after each turn (Not good?)
-#[derive(Component, Default)]
+#[derive(Component, Default, Debug)]
 pub struct UnitState {
     pub used_move: bool,
     pub used_action: bool,
 }
 
+#[derive(Component, Default, Debug, Copy, Clone)]
+pub enum Terrain {
+    #[default]
+    /// Tile is grassland or road,
+    Clear,
+    Water,
+}
 
+#[derive(Debug, Copy, Clone)]
+pub enum Ability {
+    /// Intagible units can move through all other units, walls and buildings. 
+    Intangible,
+    /// Can move through all terrain
+    /// Can move diagonally    
+    Flying,
 
+    /// `Aquatic`` units can move like `Swimmer`s
+    /// but can also end their turn on a water tile and take actions while on a water tile.
+    Aquatic,
+
+    /// `Swimmer`s move through a water tile. They can't end a turn on a water tile however.
+    Swimmer,
+
+    /// Can move a second time per turn instead of taking an action.
+    Runner,
+
+    /// Can perform two actions if they don't move this turn.
+    Operator,
+
+    /// Can move after performing an action, if they hadn't already moved this turn.
+    Tactician
+}
+
+#[derive(Component, Default, Debug, Clone)]
+pub struct Abilites(pub HashSet<Ability>);
 
