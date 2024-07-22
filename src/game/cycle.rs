996fe890af9fe9ecfdd02cfe31f050b3de3a9d55
Nixon <voxell.technologies@gmail.com>
2024-07-22 17:08:31 +0800

    feat: add cycle update events

--- src/game/cycle.rs
+++ src/game/cycle.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+pub const TURN_PER_DAY: u32 = 10;
+
 pub struct CyclePlugin;
 
 impl Plugin for CyclePlugin {
@@ -7,13 +9,57 @@ impl Plugin for CyclePlugin {
         app.init_state::<Season>()
             .init_state::<TimeOfDay>()
             .init_resource::<DayCycle>()
-            .init_resource::<Turn>();
+            .init_resource::<Turn>()
+            .add_event::<NextSeason>()
+            .add_event::<NextTurn>()
+            .add_systems(Update, (turn_update_event, season_update_event));
+    }
+}
+
+fn turn_update_event(
+    mut next_turn_evt: EventReader<NextTurn>,
+    mut turn: ResMut<Turn>,
+    day_cycle: Res<DayCycle>,
+    mut time_of_day: ResMut<NextState<TimeOfDay>>,
+) {
+    if next_turn_evt.is_empty() == false {
+        next_turn_evt.clear();
+        turn.0 = (turn.0 + 1) % TURN_PER_DAY;
+
+        if turn.0 >= day_cycle.day {
+            time_of_day.set(TimeOfDay::Night);
+        } else {
+            time_of_day.set(TimeOfDay::Day);
+        }
+    }
+}
+
+fn season_update_event(
+    mut next_season_evt: EventReader<NextSeason>,
+    season: Res<State<Season>>,
+    mut next_season: ResMut<NextState<Season>>,
+    mut day_cycle: ResMut<DayCycle>,
+) {
+    if next_season_evt.is_empty() == false {
+        next_season_evt.clear();
+
+        let season = match season.get() {
+            Season::Summer => Season::Autumn,
+            Season::Autumn => Season::Winter,
+            Season::Winter => Season::Summer,
+        };
+
+        next_season.set(season);
+        *day_cycle = DayCycle::from(season);
     }
 }
 
 /// Current turn in the day cycle.
 #[derive(Resource, Debug, Copy, Clone, PartialEq, Default)]
 pub struct Turn(pub u32);
+
+#[derive(Event, Copy, Clone, PartialEq, Default)]
+pub struct NextTurn;
 
 #[derive(States, Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
 pub enum Season {
@@ -22,6 +68,9 @@ pub enum Season {
     Autumn,
     Winter,
 }
+
+#[derive(Event, Copy, Clone, PartialEq, Default)]
+pub struct NextSeason;
 
 #[derive(States, Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
 pub enum TimeOfDay {
