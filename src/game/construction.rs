4bb13a2f961d957f537e7b8c2c13ee4dacee20e1
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2024-12-05 22:17:51 +0800

    Ui ergonomics using icons (#8)
    * Update Resolution (#1)
    
    * Update Resolution
    
    * fix doc
    
    * Create new character
    
    create a variant of the character
    
    * added character
    
    update another 2 variant of guardian
    
    * add different tiles
    
    add more different tiles
    
    * Renamed files
    
    * Move actor uis to the top, remove names
    
    * added icon
    
    added human icon
    
    * Add smaller icon variants for shop, move to left
    
    * added icon
    
    added building icon
    
    * added icon
    
    added blacksmith icon
    
    * Move building menu to top, allow cancelation
    
    * Remove SmallHouse and StrongHouse
    
    ---------
    
    Co-authored-by: Diviyan <139028711+Diviyan20@users.noreply.github.com>
    Co-authored-by: Qhimera <Herng0495@gmail.com>

--- src/game/construction.rs
+++ src/game/construction.rs
@@ -2,6 +2,8 @@ use bevy::color::palettes::css;
 use bevy::prelude::*;
 use bevy::utils::HashMap;
 use sickle_ui::prelude::*;
+use strum::IntoEnumIterator;
+use strum_macros::{AsRefStr, EnumIter};
 
 use crate::screen::Screen;
 use crate::ui::prelude::InteractionPalette;
@@ -33,42 +35,27 @@ use super::tile_set::tile_coord_translation;
 use super::tile_set::TileSet;
 use super::tile_set::TILE_ANCHOR;
 
-#[derive(Component, Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[derive(Component, EnumIter, AsRefStr, Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum StructureType {
-    SmallHouse,
     House,
-    StrongHouse,
     Tavern,
     ArcherTower,
     Blacksmith,
 }
 
 impl StructureType {
-    pub const ALL: [Self; 6] = [
-        Self::SmallHouse,
-        Self::House,
-        Self::StrongHouse,
-        Self::Tavern,
-        Self::ArcherTower,
-        Self::Blacksmith,
-    ];
-
     pub fn name(&self) -> &str {
         match self {
-            StructureType::SmallHouse => "small house",
-            StructureType::House => "house",
-            StructureType::StrongHouse => "strong house",
-            StructureType::Tavern => "tavern",
-            StructureType::ArcherTower => "tower",
-            StructureType::Blacksmith => "blacksmith",
+            StructureType::House => "House",
+            StructureType::Tavern => "Tavern",
+            StructureType::ArcherTower => "Tower",
+            StructureType::Blacksmith => "Blacksmith",
         }
     }
 
     pub fn tile_texture(&self) -> &str {
         match self {
-            StructureType::SmallHouse => "house1",
-            StructureType::House => "house1",
-            StructureType::StrongHouse => "house1",
+            StructureType::House => "house",
             StructureType::Tavern => "tavern",
             StructureType::ArcherTower => "tower",
             StructureType::Blacksmith => "blacksmith",
@@ -79,8 +66,11 @@ impl StructureType {
 #[derive(Component)]
 pub struct BuildButton(pub StructureType);
 
+#[derive(Component)]
+pub struct CancelBuildButton;
+
 pub struct StructureCost {
-    pub days: u32,
+    pub turns: u32,
     pub workers: u32,
     pub gold: u32,
     /// only one of these structures can be built
@@ -94,36 +84,18 @@ impl Default for StructureCosts {
     fn default() -> Self {
         let costs: HashMap<_, _> = [
             (
-                StructureType::SmallHouse,
-                StructureCost {
-                    days: 1,
-                    workers: 5,
-                    gold: 25,
-                    is_exclusive: false,
-                },
-            ),
-            (
                 StructureType::House,
                 StructureCost {
-                    days: 1,
+                    turns: 1,
                     workers: 10,
                     gold: 50,
                     is_exclusive: false,
                 },
             ),
             (
-                StructureType::StrongHouse,
-                StructureCost {
-                    days: 1,
-                    workers: 15,
-                    gold: 100,
-                    is_exclusive: false,
-                },
-            ),
-            (
                 StructureType::Tavern,
                 StructureCost {
-                    days: 1,
+                    turns: 1,
                     workers: 10,
                     gold: 75,
                     is_exclusive: true,
@@ -132,7 +104,7 @@ impl Default for StructureCosts {
             (
                 StructureType::ArcherTower,
                 StructureCost {
-                    days: 1,
+                    turns: 1,
                     workers: 5,
                     gold: 25,
                     is_exclusive: false,
@@ -141,7 +113,7 @@ impl Default for StructureCosts {
             (
                 StructureType::Blacksmith,
                 StructureCost {
-                    days: 1,
+                    turns: 1,
                     workers: 10,
                     gold: 125,
                     is_exclusive: true,
@@ -158,83 +130,76 @@ impl Default for StructureCosts {
 #[derive(Component)]
 pub struct BuildingPanel;
 
-pub fn building_panel_layout(mut commands: Commands, costs: Res<StructureCosts>) {
+#[derive(Component)]
+pub struct StructureDetail;
+
+pub fn building_panel_layout(mut commands: Commands) {
     commands.ui_builder(UiRoot).row(|ui| {
-        ui.insert(StateScoped(Screen::Playing))
-            .insert(BuildingPanel)
-            .style()
+        ui.insert((BuildingPanel, StateScoped(Screen::Playing)));
+        ui.style()
             .width(Val::Percent(100.))
             .height(Val::Percent(100.))
-            .justify_content(JustifyContent::End)
-            .align_items(AlignItems::Center);
+            .justify_content(JustifyContent::End);
+
         ui.column(|ui| {
             ui.style()
-                .height(Val::Auto)
-                .padding(UiRect::all(Val::Px(5.)))
-                .border(UiRect::all(Val::Px(2.)))
-                .border_color(Color::WHITE)
-                .border_radius(BorderRadius::all(Val::Px(5.)))
-                .background_color(Color::BLACK)
-                .row_gap(Val::Px(2.))
-                .flex_grow(0.)
-                .flex_shrink(0.)
-                .margin(UiRect::all(Val::Px(10.)))
-                .width(Val::Px(300.));
+                .padding(UiRect::all(Val::Px(10.)))
+                .justify_content(JustifyContent::Start);
 
-            ui.label(LabelConfig::from("Build"))
-                .style()
-                .font_size(BIG_TEXT_SIZE);
+            ui.row(|_| {}).style().height(Val::Px(60.));
 
-            for building_type in StructureType::ALL {
-                let Some(cost) = costs.get(&building_type) else {
-                    continue;
-                };
-
-                ui.container(ButtonBundle { ..default() }, |ui| {
-                    ui.insert((
-                        BuildButton(building_type),
-                        InteractionPalette {
-                            none: css::BLACK.into(),
-                            hovered: css::TEAL.into(),
-                            pressed: css::INDIAN_RED.into(),
-                        },
-                    ))
+            ui.row(|ui| {
+                ui.style().justify_content(JustifyContent::End);
+                ui.label(LabelConfig::from("Build"))
                     .style()
-                    .border(UiRect::all(Val::Px(2.)))
-                    .border_color(Color::WHITE)
-                    .padding(UiRect::all(Val::Px(5.)));
+                    .font_size(BIG_TEXT_SIZE);
+            });
 
-                    ui.row(|ui| {
-                        ui.style().justify_content(JustifyContent::SpaceBetween);
-                        ui.label(LabelConfig::from(building_type.name()))
-                            .style()
-                            .font_size(TEXT_SIZE);
+            ui.row(|ui| {
+                for building_type in StructureType::iter() {
+                    ui.container(ButtonBundle::default(), |ui| {
+                        ui.style()
+                            .flex_shrink(1.)
+                            .flex_grow(0.)
+                            .align_self(AlignSelf::End);
+
+                        ui.insert((
+                            BuildButton(building_type),
+                            InteractionPalette {
+                                none: css::BLACK.into(),
+                                hovered: css::TEAL.into(),
+                                pressed: css::INDIAN_RED.into(),
+                            },
+                        ))
+                        .style()
+                        .border(UiRect::all(Val::Px(2.)))
+                        .border_color(Color::WHITE)
+                        .padding(UiRect::all(Val::Px(4.)));
 
                         ui.column(|ui| {
-                            ui.style().justify_content(JustifyContent::End);
-                            ui.row(|ui| {
-                                ui.style().column_gap(Val::Px(10.));
-                                for (icon, value) in [
-                                    ("icons/gold_coins.png", cost.gold.to_string()),
-                                    ("icons/population.png", cost.workers.to_string()),
-                                    ("icons/hourglass.png", cost.days.to_string()),
-                                ] {
-                                    ui.row(|ui| {
-                                        ui.style()
-                                            .justify_content(JustifyContent::End)
-                                            .column_gap(Val::Px(1.));
-                                        ui.icon(icon).style().width(ICON_SIZE).height(ICON_SIZE);
-
-                                        ui.label(LabelConfig::from(value))
-                                            .style()
-                                            .font_size(TEXT_SIZE);
-                                    });
-                                }
-                            });
+                            ui.style().justify_content(JustifyContent::SpaceBetween);
+                            ui.icon("icons/".to_owned() + building_type.tile_texture() + ".png")
+                                .style()
+                                .height(Val::Px(30.))
+                                .width(Val::Px(30.));
                         });
                     });
+                }
+            });
+
+            ui.column(|ui| {
+                ui.style().padding(UiRect {
+                    left: Val::Px(20.),
+                    right: Val::Px(5.),
+                    top: Val::Px(10.),
+                    ..default()
                 });
-            }
+
+                ui.row(|ui| {
+                    ui.style().justify_content(JustifyContent::End);
+                    ui.insert(StructureDetail);
+                });
+            });
         });
     });
 }
@@ -248,6 +213,93 @@ pub fn build_btn_interaction(
         if *i == Interaction::Pressed {
             build_select.send(SelectStructureTypeEvent(b.0));
             selected_structure.0 = Some(b.0);
+        }
+    }
+}
+
+pub fn update_structure_detail(
+    mut commands: Commands,
+    q_structure_detail: Query<Entity, With<StructureDetail>>,
+    selected_structure: Res<SelectedStructueType>,
+    costs: Res<StructureCosts>,
+) {
+    let Ok(entity) = q_structure_detail.get_single() else {
+        return;
+    };
+    commands.entity(entity).despawn_descendants();
+
+    let Some(structure_type) = selected_structure.0 else {
+        return;
+    };
+
+    let Some(cost) = costs.get(&structure_type) else {
+        return;
+    };
+
+    commands.ui_builder(entity).column(|ui| {
+        ui.style()
+            .padding(UiRect::all(Val::Px(10.)))
+            .border(UiRect::all(Val::Px(2.)))
+            .border_color(Color::WHITE.with_alpha(0.3))
+            .background_color(Color::BLACK.with_alpha(0.3))
+            .row_gap(Val::Px(10.));
+
+        ui.row(|ui| {
+            ui.label(LabelConfig::from(structure_type.name()))
+                .style()
+                .font_size(TEXT_SIZE);
+        });
+
+        ui.style().row_gap(Val::Px(10.));
+        for (icon, value) in [
+            ("icons/gold_coins.png", cost.gold.to_string()),
+            ("icons/population.png", cost.workers.to_string()),
+            ("icons/hourglass.png", cost.turns.to_string()),
+        ] {
+            ui.row(|ui| {
+                ui.style().justify_content(JustifyContent::End);
+                ui.icon(icon).style().width(ICON_SIZE).height(ICON_SIZE);
+
+                ui.label(LabelConfig::from(value))
+                    .style()
+                    .font_size(TEXT_SIZE);
+            });
+        }
+
+        ui.row(|ui| {
+            ui.label(LabelConfig::from("Select tile to build."))
+                .style()
+                .font_size(TEXT_SIZE * 0.5);
+        });
+
+        ui.row(|ui| {
+            ui.container(ButtonBundle::default(), |ui| {
+                ui.label(LabelConfig::from("Cancel"))
+                    .style()
+                    .font_size(TEXT_SIZE);
+            })
+            .insert((
+                CancelBuildButton,
+                InteractionPalette {
+                    none: css::RED.into(),
+                    hovered: css::INDIAN_RED.into(),
+                    pressed: css::DARK_RED.into(),
+                },
+            ))
+            .style()
+            .padding(UiRect::all(Val::Px(4.)))
+            .border_radius(BorderRadius::all(Val::Px(4.)));
+        });
+    });
+}
+
+pub fn cancel_build_btn_interaction(
+    q_interactions: Query<&Interaction, (Changed<Interaction>, With<CancelBuildButton>)>,
+    mut selected_structure: ResMut<SelectedStructueType>,
+) {
+    for i in q_interactions.iter() {
+        if *i == Interaction::Pressed {
+            selected_structure.0 = None;
         }
     }
 }
@@ -345,7 +397,7 @@ pub fn spawn_in_progress_building(
             },
             StateScoped(Screen::Playing),
             structure_type,
-            RemainingConstructionTurns(cost.days),
+            RemainingConstructionTurns(cost.turns),
             ConstructionWorkers(cost.workers),
             BuildingSite,
             Structure,
@@ -354,7 +406,7 @@ pub fn spawn_in_progress_building(
             builder.spawn((
                 Text2dBundle {
                     text: Text::from_section(
-                        cost.days.to_string(),
+                        cost.turns.to_string(),
                         TextStyle {
                             font_size: 100.,
                             ..Default::default()
@@ -425,16 +477,9 @@ pub fn update_building_progress(
             println!("entity = {:?}", object_entity.id());
             match s {
                 StructureType::Tavern => {
-                    println!("Insert Tavern");
                     object_entity.insert(Tavern);
                 }
-                StructureType::SmallHouse => {
-                    object_entity.insert(House);
-                }
                 StructureType::House => {
-                    object_entity.insert(House);
-                }
-                StructureType::StrongHouse => {
                     object_entity.insert(House);
                 }
                 StructureType::ArcherTower => {
